85a2ce7356985cc805b7d597bb21a0827f4cfabf
etim <pongsake8832@gmail.com>
2017-11-12 22:52:39 +0700

    session 3 still error

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,23 +2,39 @@
 <div class="box">
     <h2 class="title"> App : {{ movieId}} </h2>
     <movie  @chooseMovie="handleChooseMovie" :movieId="movieId"/>
+    <Seat 
+    :movieId="movieId"
+     @chooseSeat="handleChooseSeat"
+     :selectSeats="selectSeats"
+     />
 </div>
 </template>
 
 <script>
 import Movie from 'Components/Movie.vue'
-
+import Seat from 'Components/Seat.vue'
 export default {
-    components : { Movie },
+    components : { Movie , Seat },
     data(){
         return{
-            movieId:''
+            movieId:'',
+            selectSeats:[]
         }
     },
     methods:{
         handleChooseMovie(movieId){
             console.log('ok');
             this.movieId = movieId;
+        },
+        handleChooseSeat(seat){
+      const ids = this.selectSeats.map(s => s.id);
+      const idx = ids.indexOf(seat.id)
+      if(idx === -1){
+          this.selectSeats.push(seat)
+      } else{
+          this.selectSeats.splice(idx,1);
+      }
+        
         }
     }
 }
---
+++ src/components/Seat.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="box">
+      <template v-for="s in seats" >
+        <button 
+          :class="className(s)"
+          :disabled="s.seated"
+          @click="chooseSeat(s)"
+        > {{s.id}} {{s.price}} </button>
+        <span>&nbsp:</span>
+        </template>
+        <span>{{selectSeats}} </span>
+   </div>
+</template>
+
+<script>
+import move from "../Others/movie.json";
+export default {
+  props: ["movieId", "selectSeats"],
+
+  methods: {
+    className(seat) {
+      const ids = this.selecSeats.map(s => s.id);
+      const idx = ids.indexOf(seat.id);
+
+      return ["button", { "is-danger": seat.seated, "is-primary": idx != -1 }];
+    },
+    chooseSeat(seat) {
+      this.$emit("chooseSeat", seat);
+    }
+  },
+
+  computed: {
+    seats() {
+      return movie[this.movieId];
+    }
+  }
+};
+</script>
+
